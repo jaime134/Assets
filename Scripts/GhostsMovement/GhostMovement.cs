028a74100bf9a41778ab04b6036886d7ab0426e4
aliciaaml <al394510@uji.es>
2022-10-16 20:53:55 +0200

    maquina de estados

--- Scripts/GhostsMovement/GhostMovement.cs
+++ Scripts/GhostsMovement/GhostMovement.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class Ghost3Movement : MonoBehaviour
+public class GhostMovement : MonoBehaviour
 {
     //public NavMeshAgent navMeshAgent;
 
@@ -17,14 +17,14 @@ public class Ghost3Movement : MonoBehaviour
 
     public GameObject nextWaypoint;
     public GameObject calledWaypoint;
+    public GameObject pursueWaypoint;
     GameObject lastWaypoint = null;
 
-    public static bool called;
     public static Vector3 calledPosition;
+
 
     void Start()
     {
-        called = true;  //Cambiar a false
         m_Rigidbody = GetComponent<Rigidbody>();
 
     }
@@ -32,8 +32,51 @@ public class Ghost3Movement : MonoBehaviour
     void FixedUpdate()
     {
 
-        if (called)
+        if (MaquinaEstados.called)
         {
+            Debug.Log("CALLED");
+            //Destination: calledPosition
+            if (transform.position == nextWaypoint.transform.position && transform.position != calledWaypoint.transform.position)
+            {
+                var listNeighbors = nextWaypoint.gameObject.GetComponent<Neighbors>().neighbors;
+
+                float minDist = 100000000f;
+                GameObject newWaypoint = null;
+
+                foreach (GameObject neighbor in listNeighbors)
+                {
+
+                    float dist = Mathf.Sqrt(Mathf.Pow(neighbor.transform.position.x - calledWaypoint.transform.position.x, 2) + Mathf.Pow(neighbor.transform.position.z - calledWaypoint.transform.position.z, 2));
+                    if (dist < minDist)
+                    {
+                        minDist = dist;
+                        newWaypoint = neighbor;
+                    }
+                }
+
+                lastWaypoint = nextWaypoint;
+                nextWaypoint = newWaypoint;
+
+                _direction = (nextWaypoint.transform.position - transform.position).normalized;
+                _lookRotation = Quaternion.LookRotation(_direction);
+                transform.rotation = Quaternion.Slerp(transform.rotation, _lookRotation, Time.deltaTime * turnSpeed);
+            }
+
+            transform.position = Vector3.MoveTowards(transform.position, nextWaypoint.transform.position, m_velocidad * Time.deltaTime);
+            
+            if(transform.position == calledWaypoint.transform.position){
+
+                MaquinaEstados.called = false;
+                GargoyleObserver.detected = false;
+
+            }
+        }
+
+        if (MaquinaEstados.pursue)
+        {
+
+            Debug.Log("PERSIGUIENDO");
+
             //Destination: calledPosition
             if (transform.position == nextWaypoint.transform.position)
             {
@@ -41,17 +84,17 @@ public class Ghost3Movement : MonoBehaviour
 
                 float minDist = 100000000f;
                 GameObject newWaypoint = null;
-                Debug.Log("prueba1");
+    
 
                 foreach (GameObject neighbor in listNeighbors)
                 {
-                    Debug.Log("prueba2");
-                    float dist = Mathf.Sqrt(Mathf.Pow(neighbor.transform.position.x - calledWaypoint.transform.position.x, 2) + Mathf.Pow(neighbor.transform.position.z - calledWaypoint.transform.position.z, 2));
+           
+                    float dist = Mathf.Sqrt(Mathf.Pow(neighbor.transform.position.x - pursueWaypoint.transform.position.x, 2) + Mathf.Pow(neighbor.transform.position.z - calledWaypoint.transform.position.z, 2));
                     if (dist < minDist)
                     {
                         minDist = dist;
                         newWaypoint = neighbor;
-                        Debug.Log("prueba3");
+                
                     }
                 }
 
@@ -65,12 +108,13 @@ public class Ghost3Movement : MonoBehaviour
 
             transform.position = Vector3.MoveTowards(transform.position, nextWaypoint.transform.position, m_velocidad * Time.deltaTime);
 
-
-            //called = false;
         }
 
-        else
+        if(MaquinaEstados.patrol)
         {
+
+            Debug.Log("PATRUYANDO");
+
             if (transform.position == nextWaypoint.transform.position)
             {
                 var listNeighbors = nextWaypoint.gameObject.GetComponent<Neighbors>().neighbors;
@@ -94,4 +138,7 @@ public class Ghost3Movement : MonoBehaviour
 
 
     }
+
+
+
 }
